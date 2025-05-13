f934177664d02854fb3bd5631a791cfd688bbdc7
rods <rods.desenvolvedor@gmail.com>
2025-05-13 19:30:33 -0400

    feat: base da entidade Comentario

---
+++ src/main/java/com/sistema/historia/backend/models/Comentario.java
@@ -0,0 +1,23 @@
+package com.sistema.historia.backend.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_COMENTARIO")
+public class Comentario {
+
+    @Id
+    @GeneratedValue()
+    private UUID id;
+
+    private String conteudo;
+
+    private LocalDateTime criadoEm;
+    
+}
